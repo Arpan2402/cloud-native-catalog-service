613d511b2517994471a6257e1f7f94bcd3a7dc6a
Arpan Mitra <arpan.mitra@oracle.com>
2023-05-26 17:28:00 +0530

    "Added external config with Spring Cloud Config"

--- src/main/java/com/arpanm/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/arpanm/catalogservice/CatalogServiceApplication.java
@@ -1,9 +1,12 @@
 package com.arpanm.catalogservice;
 
+import com.arpanm.catalogservice.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {ApplicationProperties.class})
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
