edb81beaf04b697f2d47c75f98742bc40a30a24f
Arpan Mitra <arpan.mitra@oracle.com>
2023-05-31 18:58:24 +0530

    Added deployment scripts to docker

--- src/test/java/com/arpanm/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/arpanm/catalogservice/CatalogServiceApplicationTests.java
@@ -3,13 +3,11 @@ package com.arpanm.catalogservice;
 import com.arpanm.catalogservice.dao.BookRepository;
 import com.arpanm.catalogservice.domain.Book;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
