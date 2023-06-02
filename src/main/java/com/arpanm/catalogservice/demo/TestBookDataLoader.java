a65b68dff3382c87e29c6f725fbad0229f787bd0
Arpan Mitra <arpan.mitra@oracle.com>
2023-06-02 18:34:19 +0530

    Added kubernetes manifests for deployment

--- src/main/java/com/arpanm/catalogservice/demo/TestBookDataLoader.java
+++ src/main/java/com/arpanm/catalogservice/demo/TestBookDataLoader.java
@@ -1,6 +1,8 @@
 package com.arpanm.catalogservice.demo;
 
 import com.arpanm.catalogservice.domain.Book;
+import com.arpanm.catalogservice.exception.BookAlreadyExistsException;
+import com.arpanm.catalogservice.exception.BookNotFoundException;
 import com.arpanm.catalogservice.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,15 +21,20 @@ public class TestBookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookData() {
-        log.info("Loading Initial Books to Catalog for testing purposes...");
-        this.bookService.addNewBookInCatalog(
-                Book.of("1234567", "Title1", "Author1", 1000d, "Publisher1"));
-        this.bookService.addNewBookInCatalog(
-                Book.of("1234568", "Title2", "Author2", 2000d, "Publisher2"));
-        this.bookService.addNewBookInCatalog(
-                Book.of("1234569", "Title3", "Author3", 3000d, "Publisher3"));
-        this.bookService.addNewBookInCatalog(
-                Book.of("2345671", "Title4", "Author4", 4000d, "Publisher4"));
-        log.info("Loading Initial Books to Catalog Complete.");
+        try{
+            log.info("Loading Initial Books to Catalog for testing purposes...");
+            this.bookService.addNewBookInCatalog(
+                    Book.of("1234567", "Title1", "Author1", 1000d, "Publisher1"));
+            this.bookService.addNewBookInCatalog(
+                    Book.of("1234568", "Title2", "Author2", 2000d, "Publisher2"));
+            this.bookService.addNewBookInCatalog(
+                    Book.of("1234569", "Title3", "Author3", 3000d, "Publisher3"));
+            this.bookService.addNewBookInCatalog(
+                    Book.of("2345671", "Title4", "Author4", 4000d, "Publisher4"));
+            log.info("Loading Initial Books to Catalog Complete.");
+        } catch (BookNotFoundException | BookAlreadyExistsException ex) {
+            log.info(ex.getMessage());
+            log.info("Books are already present in Catalog");
+        }
     }
 }
